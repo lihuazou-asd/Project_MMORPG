2a6e7090eac35192b6eb14c3d53efcf421a97da8
lihuazou-asd <877644198@qq.com>
2025-08-17 17:54:11 +0800

    生成数据库表，修改协议

--- mmorpg/Src/Server/GameServer/GameServer/Entities.Designer.cs
+++ mmorpg/Src/Server/GameServer/GameServer/Entities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// 为模型“D:\BaiduNetdiskDownload\P2【商业级MMORPG大型网游】Unity全栈开发 - 带源码课件\素材工具\mmorpg\Src\Server\GameServer\GameServer\Entities.edmx”启用了 T4 代码生成。
+﻿// 为模型“D:\UnityProjects\Project_MMORPG\mmorpg\Src\Server\GameServer\GameServer\Entities.edmx”启用了 T4 代码生成。
 // 要启用旧代码生成功能，请将“代码生成策略”设计器属性的值
 // 更改为“旧的 ObjectContext”。当在设计器中打开该模型时，此属性会出现在
 // “属性”窗口中。
--- mmorpg/Src/Server/GameServer/GameServer/Entities.edmx.sql
+++ mmorpg/Src/Server/GameServer/GameServer/Entities.edmx.sql
@@ -2,8 +2,8 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 01/14/2019 20:34:56
--- Generated from EDMX file: D:\Projects\ExtremeWorld\mmowork\Src\Server\GameServer\GameServer\Entities.edmx
+-- Date Created: 08/17/2025 17:29:45
+-- Generated from EDMX file: D:\UnityProjects\Project_MMORPG\mmorpg\Src\Server\GameServer\GameServer\Entities.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
